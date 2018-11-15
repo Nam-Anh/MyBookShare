5a4c20bfb901d48e49b5f01950ae2bc1b6d89056
MinhNV <nguyenvuminh98@gmail.com>
2018-11-15 09:00:22 +0700

    init project

---
+++ View/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿//using Entity;
+using System;
+using System.Drawing;
+
+namespace BookShare
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // set title for page
+            Page.Title = "Login - BookShare";
+
+            if (Session["username"] != null && Request.QueryString["command"] != null)
+            {
+                Session.RemoveAll();
+            }
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            userservice.userservice service = new userservice.userservice();
+            userservice.user user = service.checklogin(request.form["usernametxt"], request.form["passwordtxt"]);
+
+            if (user == null) errortxt.text = "username or password is incorrect";
+            else
+            {
+                errortxt.text = user.fullname;
+                session.add("currentuser", user);
+                session.add("username", user.username);
+                response.redirect("home.aspx");
+            }
+        }
+    }
+}
